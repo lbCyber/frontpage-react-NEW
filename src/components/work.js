7d1047e269693ce5c030eb23916d9d19a9a49b1b
lbCyber <paul@lintbox.com>
2020-10-06 16:15:03 -0400

    Added portfolio json and simple mapping of its elements (also fixed package_lock which was broken uggghh

--- src/components/work.js
+++ src/components/work.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown/with-html';
+import Project from '../json/projects.json';
 
 class Work extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentPage: 3
+      currentPage: 3,
+      work: Project.projects,
+      months: Project.months,
+      features: Project.featureList
     }
   }
 
@@ -15,7 +20,33 @@ class Work extends Component {
   render() {
     return (
       <div>
-        work
+        {
+          this.state.work.map((project, key) => {
+            return (
+              <div key={key}>
+                <p>Name: {project.title}</p>
+                <img src={`./assets/${project.image}`} alt="" />
+                <p>Published: {`${this.state.months[project.published[1]]} ${project.published[2]}, ${project.published[0]}`}</p>
+                <div>
+                  <ReactMarkdown source={project.text} escapeHtml={false} />
+                </div>
+                <div>
+                  <ul>
+                    {
+                      project.features.map((feature, key) => {
+                        return (
+                          <li key={key}>
+                            {this.state.features[feature]}
+                          </li>
+                        )
+                      })
+                    }
+                  </ul>
+                </div>
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
